ddb2d13bace9794f7d2dc0bd05b3e7f8e4ba596f
Brenan Mota <brenanmota1@gmail.com>
2023-09-20 18:30:54 -0700

    half of the complete challenge

---
+++ main.js
@@ -0,0 +1,45 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+alert('Hello Word!!!')
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+const numberOne = 246
+const numberTwo = 769
+
+const sum = numberOne + numberTwo
+alert(sum)
+
+//3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+const isNumber = 'ABC'
+
+if(typeof isNumber == 'number'){
+  alert('É um número')
+} else {
+  alert('Não é um número')
+}
+
+
+//4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+const isString = 'ABC'
+
+if(typeof isString == 'string'){
+  alert('É uma string')
+} else {
+  alert('Não é uma string')
+}
+
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+const isBoolean = false
+
+if (typeof isBoolean == 'boolean') {
+  alert('É um booleano')
+} else {
+  alert('Não é um booleano')
+}
+
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
